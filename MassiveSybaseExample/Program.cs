dfd81e43efe223bd556a102d7d1cbe8914b48cc6
Jason Motylinski <jason.motylinski@thomsonreuters.com>
2012-06-19 14:28:25 -0500

    Initial commit

---
+++ MassiveSybaseExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace Massive.Sybase.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var users = DB.Current.Query("select * from user_info");
+
+           System.Console.WriteLine("Massive Results...");
+           Thread.Sleep(4000);
+           
+            foreach (var u in users)
+           {
+               System.Console.WriteLine(u.user_id);
+           }
+
+            System.Console.WriteLine("Done!");
+            System.Console.ReadKey();
+        }
+    }
+}
